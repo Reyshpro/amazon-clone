a0ff1e4777ddf0f102ad148a831edf0c8b513662
Reyyen <reyyensehebi@gmail.com>
2025-09-12 16:00:52 +0300

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,7 +78,12 @@ button.addEventListener('click',()=>{
     productId: productId,
     quantity: 1
   });}
+
+let cartQuantity= 0;
+ cart.forEach((item)=>{
+ cartQuantity += item.quantity;
+ });
+ document.querySelector('.js-cart-quantity').innerHTML= cartQuantity;
  
-  console.log(cart);
 });
 });
